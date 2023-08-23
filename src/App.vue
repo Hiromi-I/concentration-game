2cd8852d6d2af60c3c7b71bbbafff2bd4098712e
hiro <hiromi.iwai.04.03@gmail.com>
2023-08-23 19:53:20 +0900

    スコアを仮データから書き換え

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import CardItem from "@/components/CardItem.vue";
 import { useCards } from "@/stores/cards";
+import { useScores } from "@/stores/scores";
 
 const { cards, turnCard } = useCards();
+const { trialCount, pairedCount } = useScores();
 
 const onCardClick = (index: number) => {
   turnCard(index);
@@ -16,8 +18,8 @@ const onCardClick = (index: number) => {
     </div>
     <div class="score-container">
       <p class="score">
-        TRY: 000<br />
-        SCORE: 000
+        TRY: {{ trialCount }}<br />
+        SCORE: {{ pairedCount }}
       </p>
     </div>
   </header>
