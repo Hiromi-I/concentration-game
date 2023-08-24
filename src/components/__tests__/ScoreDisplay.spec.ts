a2f6c819b804b30770893997af890ea3e3caeeed
hiro <hiromi.iwai.04.03@gmail.com>
2023-08-24 22:39:04 +0900

    スコア表示をテスト

---
+++ src/components/__tests__/ScoreDisplay.spec.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { createTestingPinia } from "@pinia/testing";
+import { render } from "@testing-library/vue";
+
+import ScoreDisplay from "@/components/ScoreDisplay.vue";
+
+
+describe("ScoreDisplay", () => {
+
+  beforeEach(() => {
+    const initialState = { "score": {
+      _trialCount: 3,
+      _pairedCount: 2,
+    }};
+    createTestingPinia({ initialState });
+  });
+
+  it("renders 2 score", () => {
+    const { getByText }  = render(ScoreDisplay);
+    const pElement = getByText(/^TRY:/);
+    
+    expect(pElement.textContent).toMatch(/TRY: 003/);
+    expect(pElement.textContent).toMatch(/SCORE: 002/);
+  });
+});
