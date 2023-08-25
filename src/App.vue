c6d86f46127503cc860ae2031885cb4beb5ba443
hiro <hiromi.iwai.04.03@gmail.com>
2023-08-25 22:59:48 +0900

    ロゴの余白を調整

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,6 @@ import GameClear from '@/components/GameClear.vue'
 <style lang="css">
 .logo-container {
   background-color: black;
-  padding-bottom: 10px;
 }
 
 .logo {
@@ -29,6 +28,7 @@ import GameClear from '@/components/GameClear.vue'
   font-family: 'Monofett', cursive;
   font-size: 60px;
   margin: 0 auto;
+  padding: 20px 0 30px;
   width: var(--base-width);
 }
 
